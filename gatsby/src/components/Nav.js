657b147877b84dc6421e9539ab4ad06fa39b7345
Mike Laurel <mlaurel@hey.com>
2020-11-05 14:54:52 -0800

    Adds pizza schema files

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -1,9 +1,52 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styled from 'styled-components'
+import Logo from './Logo'
+
+const NavStyles = styled.nav`
+    .logo {
+        transform: translateY(-25%);
+    }
+    ul {
+        display: grid;
+        grid-template-columns: 1fr 1fr auto 1fr 1fr;
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        text-align: center;
+    }
+    li {
+        --rotate: -2deg;
+        transform: rotate(var(--rotate));
+        align-self: center;
+        order: 1;
+    }
+    li:nth-child(1) {
+        --rotate: 1deg;
+    }
+    li:nth-child(2) {
+        --rotate: -2.5deg;
+    }
+    li:nth-child(4) {
+        --rotate: 2.5deg;
+    }
+    li:nth-child(5) {
+        --rotate: 1deg;
+    }
+    a {
+        display: inline-block;
+        text-decoration: none;
+        font-size: 3rem;
+        &:hover {
+            color: var(--red);
+            /* transform: rotate(5deg); */
+        }
+    }
+`
 
 export default function Nav() {
     return (
-        <nav>
+        <NavStyles>
             <ul>
                 <li>
                     <Link to="/">Hot Now</Link>
@@ -12,7 +55,9 @@ export default function Nav() {
                     <Link to="/pizzas">Pizza Menu</Link>
                 </li>
                 <li>
-                    <Link to="/">LOGO</Link>
+                    <Link to="/">
+                        <Logo />
+                    </Link>
                 </li>
                 <li>
                     <Link to="/beers">Beers</Link>
@@ -21,6 +66,6 @@ export default function Nav() {
                     <Link to="/slicemasters">SliceMasters</Link>
                 </li>
             </ul>
-        </nav>
+        </NavStyles>
     )
 }
--- gatsby/src/pages/beers.js
+++ gatsby/src/pages/beers.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { styled } from 'styled-components'
 
 export default function BeersPage() {
-    return (
-        <>
-            <h1>Beers</h1>
-        </>
-    )
+	return (
+		<>
+			<h1>Beers</h1>
+		</>
+	)
 }
