e3334c7de4cbe0922f44342b4626cf3dc974e23f
Mike Laurel <mlaurel@hey.com>
2020-11-05 19:17:01 -0800

    Adds schema preview for toppings in Sanity

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -1,4 +1,5 @@
 import { MdLocalPizza as icon } from 'react-icons/md';
+import topping from './topping';
 
 export default {
     // Computer name
@@ -46,4 +47,26 @@ export default {
             of: [{ type: 'reference', to: [{ type: 'topping' }] }],
         },
     ],
+    preview: {
+        select: {
+            title: 'name',
+            media: 'image',
+            topping0: 'toppings.0.name',
+            topping1: 'toppings.1.name',
+            topping2: 'toppings.2.name',
+            topping3: 'toppings.3.name',
+        },
+        prepare: ({ title, media, ...toppings }) => {
+            console.log(title, media, toppings);
+            //  1. filter undefined toppings out
+            const tops = Object.values(toppings).filter(Boolean);
+            //  2. return the preview object for the pizza
+            return {
+                title,
+                media,
+                // tops,
+                subtitle: Object.values(tops).join(', '),
+            };
+        },
+    },
 };
--- sanity/schemas/topping.js
+++ sanity/schemas/topping.js
@@ -27,8 +27,8 @@ export default {
             name: 'name',
             vegetarian: 'vegetarian',
         },
-        prepare: (fields) => ({
-            title: `${fields.name} ${fields.vegetarian ? ' 🌱 ' : ' 🍖'}`,
+        prepare: ({ name, vegetarian }) => ({
+            title: `${name} ${vegetarian ? ' 🌱 ' : ' 🍖'}`,
         }),
     },
 };
