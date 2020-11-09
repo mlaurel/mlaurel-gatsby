4ae8cf937e3b65b5a83c4788f564e3dcfca664d4
Mike Laurel <mlaurel@hey.com>
2020-11-09 12:52:56 -0800

    Adds grid and subgrid styles to PizzaList component and pizza page

--- gatsby/src/components/PizzaList.js
+++ gatsby/src/components/PizzaList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Img from 'gatsby-image'
+import styled from 'styled-components'
 
 //  Topping Component Practice... using topping component to loop over
 //  See one line join below... more optimal
@@ -15,9 +16,32 @@ import Img from 'gatsby-image'
 //     })
 // }
 
+const PizzaGridStyles = styled.ul`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+    grid-gap: 4rem;
+    grid-auto-rows: auto;
+`
+
+const PizzaStyles = styled.li`
+    display: grid;
+    /* Take your row sizing not from the pizzaStyles div, but from the PizzaGridStyles grid */
+    @supports not (grid-template-rows: subgrid) {
+        --rows: auto auto 1fr;
+        --rowSpan: 3;
+    }
+    grid-template-rows: var(--rows, subgrid);
+    grid-row: span var(--rowSpan, 3);
+    grid-gap: 1rem;
+    h2,
+    p {
+        margin: 0;
+    }
+`
+
 function SinglePizza({ pizza }) {
     return (
-        <div>
+        <PizzaStyles>
             <Link to={`/pizza/${pizza.slug.current}`}>
                 <h2>
                     <span className="mark">{pizza.name}</span>
@@ -26,20 +50,20 @@ function SinglePizza({ pizza }) {
                     {/* <Toppings toppings={pizza.toppings} /> */}
                     {pizza.toppings.map((topping) => topping.name).join(', ')}
                 </p>
-                <Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
             </Link>
-        </div>
+            <Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
+        </PizzaStyles>
     )
 }
 export default function PizzaList({ pizzas }) {
     return (
         <>
             <p>There are {pizzas.length} pizzas total!</p>
-            <ul>
+            <PizzaGridStyles>
                 {pizzas.map((pizza) => {
                     return <SinglePizza key={pizza.id} pizza={pizza} />
                 })}
-            </ul>
+            </PizzaGridStyles>
         </>
     )
 }
---
+++ gatsby/src/components/ToppingsFilter.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function ToppingsFilter() {
+    // 1. Get a list of all the toppings
+    // 2. Get a list of all the Pizzas with their toppings
+    // 3. Count how many pizzas are in each topping
+    // 4. Loop over the list of toppings and display the topping and the count of pizzas in that topping
+    // 5. Link it up...
+
+    return (
+        <div>
+            <p>Toppings</p>
+        </div>
+    )
+}
