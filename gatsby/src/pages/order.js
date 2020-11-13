bf3c288c28d1bfa4ff5dc2e1c14afb5de42c0950
Mike Laurel <mlaurel@hey.com>
2020-11-13 11:46:20 -0800

    Adds order logic and styles, including price utils

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -1,24 +1,39 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 import useForm from '../utils/useForm'
 import SEO from '../components/SEO'
+import styled from 'styled-components'
+import Img from 'gatsby-image'
+import calculatePizzaPrice from '../utils/calculatePizzaPrice'
+import formatMoney from '../utils/formatMoney'
+import {
+    OrderForm,
+    MenuGrid,
+    PriceGrid,
+    ButtonGrid,
+} from '../styles/OrderStyles'
 
-export default function OrderPage() {
+export default function OrderPage({ data }) {
     const { values, updateValue } = useForm({
         name: '',
         email: '',
     })
-    console.log(values, updateValue)
+
+    // console.log(values, updateValue)
+    const pizzas = data.pizzas.nodes
+    console.log(pizzas)
 
     return (
         <>
             <SEO title="Order a Pizza" />
-            <form>
+            <OrderForm>
                 <fieldset>
                     <legend>Your Info</legend>
                     <label htmlFor="name">Name</label>
                     <input
                         type="text"
                         name="name"
+                        id="name"
                         value={values.name}
                         onChange={updateValue}
                     />
@@ -26,17 +41,64 @@ export default function OrderPage() {
                     <input
                         type="text"
                         name="email"
+                        id="email"
                         value={values.email}
                         onChange={updateValue}
                     />
                 </fieldset>
                 <fieldset>
                     <legend>Menu</legend>
+                    {pizzas.map((pizza) => (
+                        <MenuGrid key={pizza.id}>
+                            <Img
+                                fluid={pizza.image.asset.fluid}
+                                alt={pizza.name}
+                            />
+                            <PriceGrid>
+                                <h2>{pizza.name}</h2>
+                                <ButtonGrid>
+                                    {['S', 'M', 'L'].map((size, index) => (
+                                        <button key={index} type="button">
+                                            {size} &nbsp;
+                                            {formatMoney(
+                                                calculatePizzaPrice(
+                                                    pizza.price,
+                                                    size
+                                                )
+                                            )}
+                                        </button>
+                                    ))}
+                                </ButtonGrid>
+                            </PriceGrid>
+                        </MenuGrid>
+                    ))}
                 </fieldset>
                 <fieldset>
                     <legend>Order</legend>
                 </fieldset>
-            </form>
+            </OrderForm>
         </>
     )
 }
+
+export const query = graphql`
+    query {
+        pizzas: allSanityPizza {
+            nodes {
+                name
+                id
+                slug {
+                    current
+                }
+                price
+                image {
+                    asset {
+                        fluid(maxWidth: 100) {
+                            ...GatsbySanityImageFluid
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
---
+++ gatsby/src/styles/OrderStyles.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components'
+
+//  Exporting individual styles like this kind of sucks
+//  A better way would be to export one pageStyle with nested selectors
+
+export const OrderForm = styled.form`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 1rem;
+    legend {
+        font-size: 1.25rem;
+    }
+    fieldset:first-child {
+        grid-column: 1 / -1;
+        display: grid;
+        grid-template-columns: 1fr;
+        grid-gap: 1rem;
+    }
+    fieldset {
+    }
+`
+
+export const MenuGrid = styled.div`
+    display: grid;
+    grid-template-columns: 100px 1fr;
+    grid-gap: 1rem;
+    margin-bottom: 2rem;
+`
+
+export const PriceGrid = styled.div`
+    display: grid;
+    grid-template-rows: 1fr 1fr;
+    h2 {
+        align-self: center;
+        margin: 0;
+        padding: 0;
+    }
+`
+
+export const ButtonGrid = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 15px;
+    button {
+        font-size: 1.5rem;
+        margin: 0;
+        padding: 0;
+        border-radius: 8px;
+    }
+`
