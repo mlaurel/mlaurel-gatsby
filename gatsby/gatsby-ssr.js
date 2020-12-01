e527f9de16522beb61ba6787a42d54b34ed3b782
Mike Laurel <mlaurel@hey.com>
2020-12-01 10:35:38 -0800

    Adds daily.co video chat functionality

--- gatsby/gatsby-ssr.js
+++ gatsby/gatsby-ssr.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import Layout from './src/components/Layout'
 import { OrderProvider } from './src/components/OrderContext'
-
 export function wrapPageElement({ element, props }) {
     return <Layout {...props}>{element}</Layout>
 }
 
 export function wrapRootElement({ element }) {
     return <OrderProvider>{element}</OrderProvider>
+}
+
+export const onRenderBody = ({ setHeadComponents }) => {
+    setHeadComponents([
+        <script crossorigin src="https://unpkg.com/@daily-co/daily-js" />,
+        <script
+            key="daily_chat"
+            dangerouslySetInnerHTML={{
+                __html: `
+            allFrame = window.DailyIframe.createFrame();
+            callFrame.join({ url: 'https://mdog.daily.co/hello-daily' });
+            `,
+            }}
+        />,
+    ])
 }
